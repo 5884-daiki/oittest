792686f52cca3c1cf28a237b4b572517544ac3b6
5884-daiki <130972031+5884-daiki@users.noreply.github.com>
2023-07-16 23:21:58 +0900

    Add files via upload

--- oitconnect/config/settings.py
+++ oitconnect/config/settings.py
@@ -121,8 +121,11 @@ USE_TZ = True
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# 静的ファイルのURLパス
+STATIC_URL = '/static/'
+
+# collectstaticコマンドの出力先を指定する設定
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
